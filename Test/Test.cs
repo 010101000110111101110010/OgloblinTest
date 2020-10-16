dca951bcf7e984f206ebd0668548bae1e3f650de
010101000110111101110010 <maks.ogloblin.2002@mail.ru>
2020-10-16 17:12:49 +0900

    Initial commit

---
+++ Test/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InspectorLib;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionInsp.GetInspector();
+            FunctionInsp.GetСarInspection();
+            FunctionInsp.GetWorker();
+            FunctionInsp.SetInspector();
+            FunctionInsp.GetInspector();
+            FunctionInsp.AddWorker();
+            FunctionInsp.GetWorker();
+            FunctionInsp.GenerateNumber(123, "а", 75);
+            FunctionInsp.GenerateNumber(123, "а", 85);
+            Console.ReadLine();
+        }
+    }
+}
